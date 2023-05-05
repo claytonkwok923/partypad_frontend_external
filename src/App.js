4ffdec10d38fef4e38181759e6e637292b29b8cb
Clayton kwok <claytonkwok923@gmail.com>
2023-05-05 16:42:53 +0800

    origin react app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
-
-import React, { useEffect } from "react";
-import "./common.scss";
-import moment from "moment";
-
-
-
-import MainPage from "./form/Partypad/MainPage.js";
-import Dashboard from "./form/Partypad/Dashboard";
-import Listings from "./form/Partypad/Listings";
-import Reservations from "./form/Partypad/Reservations";
-import CreateRoomForm from "./form/Partypad/CreateRoomForm";
-import UpdateRoomForm from "./form/Partypad/UpdateRoomForm";
-import BookingPage from "./form/Partypad/BookingPage";
-import Trips from "./form/Partypad/Trips";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    //change Date.prototype.toJSON for final submitting
-    Date.prototype.toJSON = function () {
-        return moment(this).format();
-    };
-
-
-
-    return (
-
-        <BrowserRouter basename="/partypad_external">
-            <Switch>
-
-                {/* my edit - start */}
-
-                <Route exact path="/" component={MainPage} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/listings" component={Listings} />
-                <Route exact path="/reservations" component={Reservations} />
-                <Route exact path="/trips" component={Trips} />
-                <Route exact path="/createRoomForm" component={CreateRoomForm} />
-                <Route exact path="/updateRoomForm/:id" component={UpdateRoomForm} />
-                <Route exact path="/rooms/:id" component={BookingPage} />
-
-
-                {/* my edit - end  */}
-
-
-            </Switch>
-        </BrowserRouter>
-
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
