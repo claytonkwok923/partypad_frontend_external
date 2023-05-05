88377f1d567a0721dc5184e6039737eaae0965c8
Clayton kwok <claytonkwok923@gmail.com>
2023-05-05 16:57:43 +0800

    origin react fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacttt
         </a>
       </header>
     </div>
